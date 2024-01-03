d72a85323855d333ccc8b1902109890ba81de6f3
Barry <yihengzhang001@163.com>
2024-01-04 00:26:09 +0800

    代码上传，仍然面临着收敛的问题。

--- registration/optim-algorithm/test.py
+++ registration/optim-algorithm/test.py
@@ -1,17 +1,22 @@
-# Example 2D array A
-A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-# Define row and column ranges
-start_row, end_row = 1, 4  # Example row range
-start_col, end_col = 1, 5  # Example column range
 
-# Define the fill value for out-of-bounds indices
-fill_value = -1
+import torch
 
-# Create the 2D array B
-B = [[A[i][j] if i < len(A) and j < len(A[i]) else fill_value 
-      for j in range(start_col, end_col)] 
-     for i in range(start_row, end_row)]
+# 生成初始参数规定范围，
+minV = [0, 0]
+maxV = [200, 300]
 
-for row in B:
-    print(row)
+def constrain(t):
+    item_num = t.numel()
+    if t[0] < 0 or t[1] < 0:
+        print("x less zero!")
+
+    for i in range(item_num):
+        if t[i] < minV[i]:
+            t[i] = maxV[i] - (minV[i] - t[i])
+        elif t[i] > maxV[i]:
+            t[i] = minV[i] + (t[i] - maxV[i])
+    return t
+
+test = torch.tensor([1.0, 1.0])
+print(list(range(10)))
