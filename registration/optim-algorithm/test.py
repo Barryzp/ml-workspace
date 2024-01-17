41467a35c3feeba90ed9e1d47d0a5ea686eb3fef
Barry <yihengzhang001@163.com>
2024-01-17 21:46:14 +0800

    精英保留策略；另外，之前的BUG也改了，原来是没有保存每一轮的最大值，值保留了外层循环中的最后的比上一次最大值大的那个坐标。

--- registration/optim-algorithm/test.py
+++ registration/optim-algorithm/test.py
@@ -2,21 +2,7 @@
 
 import torch
 
-# 生成初始参数规定范围，
-minV = [0, 0]
-maxV = [200, 300]
 
-def constrain(t):
-    item_num = t.numel()
-    if t[0] < 0 or t[1] < 0:
-        print("x less zero!")
+t = torch.rand([2, 5])
 
-    for i in range(item_num):
-        if t[i] < minV[i]:
-            t[i] = maxV[i] - (minV[i] - t[i])
-        elif t[i] > maxV[i]:
-            t[i] = minV[i] + (t[i] - maxV[i])
-    return t
-
-test = torch.tensor([1.0, 1.0])
-print(list(range(10)))
+t.clone()
