51b2ab0bb464115757a104f35f1b2bb001c94e08
Barryzp <yihengzhang001@163.com>
2024-04-10 09:45:54 +0800

    修复夸维配准时出现的问题。

--- registration/implement/main.py
+++ registration/implement/main.py
@@ -1,9 +1,13 @@
 from registration_framework import Registration
 from pso_optim import PSO_optim
-from config import Config
+from utils.tools import Tools
 
-config = Config()
+config = Tools.load_yaml_config("config.yaml")
+res_path = f"{config.data_save_path}/{config.record_id}"
+file_name = f"{config.record_id}_config.yaml"
+Tools.save_obj_yaml(res_path, file_name, config)
+
 registration = Registration(config)
 pso = PSO_optim(config)
 registration.set_optim_algorithm(pso)
-registration.registrate()
+result = registration.registrate()
