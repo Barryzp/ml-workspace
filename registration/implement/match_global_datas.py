b04c11b6347b0c7189be6e5a7494cb26d9f07985
Barryzp <yihengzhang001@163.com>
2024-04-26 10:59:19 +0800

    解决冲突

--- registration/implement/match_global_datas.py
+++ registration/implement/match_global_datas.py
@@ -1,16 +1,19 @@
 import torch
+from utils.tools import Tools
 
 class GlobalMatchDatas():
 
-    def __init__(self) -> None:
+    def __init__(self, config) -> None:
         self.aim_slice_index = -1
         self.global_best_value = -1000
+        self.iteration_count = 0
         self.global_best_position = torch.tensor([.0, .0, .0])
         self.global_best_img = None
         self.share_records_out = []
-        self.threshold = 0.086
+        self.threshold = config.matched_threshold
         
         self.stop_loop = False
+        self.config = config
 
     # 这个best_val都是正值
     def set_best(self, best_val, best_position, best_img, ct_slice_index):
@@ -19,10 +22,19 @@ class GlobalMatchDatas():
             self.global_best_position = best_position
             self.global_best_img = best_img
             self.aim_slice_index = ct_slice_index
+            self.iteration_count += 1
             print(f"best val: {best_val}; ct_slice_index: {ct_slice_index}")
+            # 把这张图片保存一下
+            self.save_best_match()
         
         if self.global_best_value > self.threshold:
             self.stop_loop = True
+
+    # 保存最佳图像
+    def save_best_match(self):
+        file_path = Tools.get_save_path(self.config)
+        file_name = f"best_match_{self.aim_slice_index}_{self.iteration_count}.bmp"
+        Tools.save_img(file_path, file_name, self.global_best_img)
 
     def get_loop_state(self):
         return self.stop_loop
